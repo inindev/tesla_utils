e30ee06edddb87bf9d6aa604771733017ad0d03b
John Clark <inindev@gmail.com>
2025-03-01 11:22:26 -0500

    android app: update app icon
    Signed-off-by: John Clark <inindev@gmail.com>

--- app/android/tesla_app/app/src/main/java/com/github/inindev/teslaapp/ui/theme/Color.kt
+++ app/android/tesla_app/app/src/main/java/com/github/inindev/teslaapp/ui/theme/Color.kt
@@ -10,7 +10,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val TeslaRed = Color(0xFFCC0000)
+val TeslaRed = Color(0xFFC8102E)
+val TeslaSilver = Color(0xFFD8D8D8)
 val TeslaBlue = Color(0xFF3E6AE1)
 val TeslaPrimary = TeslaBlue
 
